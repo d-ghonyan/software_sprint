ec7353993633709fe6d883281418d95d9b431afa
raghonya <david.dghonyan02@gmail.com>
2022-12-18 00:21:57 +0400

    show event list on admin page

--- server/public/script.js
+++ server/public/script.js
@@ -1,26 +1,44 @@
-const form = document.querySelector('form')
-const address = document.querySelector('#address')
+const form = document.querySelector('form'),
+	title = document.querySelector('#title'),
+	events = document.querySelector('#events');
 
-const msg1 = document.querySelector('#msg1')
-const msg2 = document.querySelector('#msg2')
+const createDiv = (data) => {
+	let div = document.createElement('div');
+	let title1 = document.createElement('div');
+	let description = document.createElement('div');
+	let date = document.createElement('div');
+	let student_info = document.createElement('div');
+	let update = document.createElement('button');
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const location = address.value
-    msg1.textContent='Loading...'
-    msg2.textContent=""
-    fetch(`/weather?address=${location}`).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error){
-                msg1.textContent = data.error    
-            }else{
-                msg1.textContent=data.location
-                msg2.textContent=data.forecast
-            }
-            
-        })
-    })
+	div.classList.add("event_container");
+	title1.textContent = `title: ${data.title}`;
+	description.textContent = `description: ${data.description}`;
+	date.textContent = `date: ${data.date}`;
+	student_info.textContent = `student_info: ${data.username} ${data.email}`;
+	update.innerText = "Accept event";
+	div.append(title1, description, date, student_info, update);
+	return (div);
+}
+
+// form.addEventListener('submit', (e) => {
+	// e.preventDefault()
+
+fetch('/events').then((response) => {
+	response.json().then((data) => {
+		console.log(data);
+		if (data.error) {
+			title.textContent = data.error;
+		} else {
+			values = Object.values(data);
+			for (let i = 0; i < values.length; i++)
+			{
+				console.log(values[i]);
+				events.append(createDiv(values[i]));
+			}
+		}
+	})
 })
- 
-   
+// })
+
+
 
